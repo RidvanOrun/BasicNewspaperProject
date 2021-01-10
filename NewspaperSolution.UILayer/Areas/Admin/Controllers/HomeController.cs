01f9d33a613ceee6c8e756bbc9d82db2d8278482
ridvanorun@outlook.com <ridvanorun@outlook.com>
2021-01-10 16:16:20 +0300

    AdminHomePage

---
+++ NewspaperSolution.UILayer/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using NewspaperSolution.DataAccessLayer.Repository.Concrete.EfRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewspaperSolution.UILayer.Areas.Admin.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        EfPostRepository _postREpository;
+        public HomeController()
+        {
+            _postREpository = new EfPostRepository(); 
+        }
+
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
